fde0fb4d999a3e9a6b82c9be2f86f32fd2161c16
Ludmila <milasasha@gmail.com>
2022-07-19 17:18:45 +0300

    feat code

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor () {
+    this.massiv = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.massiv;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const penqueue = (point, value) => {
+      if (!point) {
+          return new ListNode(value)
+      }
+      point.next = penqueue(point.next, value)
+      return point;
+    }
+    this.massiv = penqueue(this.massiv, value)
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const res = this.massiv;
+    console.log(res);
+    this.massiv = res.next;
+    console.log(res.value);
+    return res.value;
   }
 }
 
@@ -35,3 +45,8 @@ module.exports = {
   Queue
 };
 
+const queue = new Queue();
+ queue.enqueue(1); // adds the element to the queue
+ queue.enqueue(3); // adds the element to the queue
+ queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ queue.getUnderlyingList() // returns { value: 3, next: null }
