6a71b7912a7757b51209de713a935c2720ee12c6
Ludmila <milasasha@gmail.com>
2022-07-19 15:46:34 +0300

    feat code

--- src/queue.js
+++ src/queue.js
@@ -34,3 +34,4 @@ class Queue {
 module.exports = {
   Queue
 };
+
--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.massiv = [];
+  }
+  push(element) {
+    this.massiv.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.massiv.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.massiv[this.massiv.length - 1];
   }
 }
 
 module.exports = {
   Stack
 };
+/*const stack = new Stack();
+stack.push(1); // adds the element to the stack
+stack.peek(); // returns the peek, but doesn't delete it, returns 1
+stack.pop(); // returns the top element from stack and deletes it, returns 1
+stack.pop(); // undefined*/
